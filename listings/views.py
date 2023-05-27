b0abaffafcbffcede20f45e1d659112e5ba123e5
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2023-05-27 18:50:49 +0200

    set up search page results and search form functionalities

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .choices import price_choices, bedroom_choices, county_choices
 
 # Create your views here.
 def index(request):
@@ -11,13 +12,30 @@ def index(request):
     page_listings = paginator.get_page(page)
     
     context = {
-        'listings': page_listings
+        'listings': page_listings,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'county_choices': county_choices,
     }
 
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    # display 404 page if id doesn't exist
+    #listing_id is coming from the urls.py being passed through the url
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+        'county_choices': county_choices,
+    }
+    return render(request, 'listings/search.html', context)
