befab6fea73c79d3a7e23ddeb0b5d57fbd54fe1d
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2023-05-29 18:23:50 +0200

    sign up validations created for email, username

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            #check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email already exists')
+                    return redirect('register')
+                else:
+                    pass
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        #register user
+        pass
+    else:
+        return render(request, 'accounts/login.html')
+
 
 def logout(request):
     return redirect(request, 'index')
