5b62e3f029f5cc67f6a4212072c565fb5eb1949f
christianmartinroffey <81240124+christianmartinroffey@users.noreply.github.com>
2023-05-31 19:53:56 +0200

    email send functionality set up for contact form

--- contacts/models.py
+++ contacts/models.py
@@ -11,6 +11,5 @@ class Contact(models.Model):
     message = models.TextField(blank=True)
     contact_date = models.DateTimeField(default=datetime.now, blank=True)
     user_id = models.IntegerField(blank=True)
-    realtor_email = models.CharField(max_length=100)
     def __str__(self):
         return self.name
